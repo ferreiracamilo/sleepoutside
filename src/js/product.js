8e3efc28863f32c5c413fb233f21f092924a7591
ferreiracamilo <camiloferreira2009@hotmail.com>
2024-11-09 00:29:27 -0300

    Fix addProductToCart so local-storage can save multiple products
    Enforce so-cart to be a list and import getLocalStorage function

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
+import { getLocalStorage } from "./utils.mjs";
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //If there's no local storage property array will be enforced to utilize push function later
+  let cart = getLocalStorage("so-cart") || [];
+
+  cart.push(product);
+
+  setLocalStorage("so-cart",cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
